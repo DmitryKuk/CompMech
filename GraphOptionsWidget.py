0447dfe243aa7cb89438154f916b3eb49f79359b
DmitryKuk <d1021976@yandex.ru>
2015-10-08 23:05:52 +0300

    Labels and axis major fix! Small DetailWindow fix

--- GraphOptionsWidget.py
+++ GraphOptionsWidget.py
@@ -20,7 +20,7 @@ class GraphOptionsWidget(Frame):
 		var = IntVar()
 		if value is not None and bool(value): var.set(1)
 		
-		cb = Checkbutton(self, text = text, variable = var, command = self.onCBClicked)
+		cb = Checkbutton(self, text = text, variable = var, command = self.onCBClicked, fg = "red")
 		if cbState in (NORMAL, DISABLED, ACTIVE): cb["state"] = cbState
 		cb.pack(fill = X)
 		
